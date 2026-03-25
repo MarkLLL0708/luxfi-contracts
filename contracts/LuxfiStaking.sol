b1c52139f0236d8ddddb65c4b516a1456fb88452
MarkLLL0708 <marklyy0708@gmail.com>
2026-03-25 14:18:36 +0800

    Create LuxfiStaking.sol

---
+++ contracts/LuxfiStaking.sol
@@ -0,0 +1,69 @@
+```solidity
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IFeeDistributor {
+    function registerStaker(address staker, uint256 amount) external;
+    function unregisterStaker(address staker) external;
+}
+
+contract LuxfiStaking is ReentrancyGuard, Ownable {
+
+    IERC20 public immutable luxfiToken;
+    IFeeDistributor public feeDistributor;
+
+    mapping(address => uint256) public staked;
+    uint256 public totalStaked;
+
+    event Staked(address indexed user, uint256 amount);
+    event Unstaked(address indexed user, uint256 amount);
+
+    constructor(address _token, address _feeDistributor) {
+        require(_token != address(0), "Invalid token");
+        require(_feeDistributor != address(0), "Invalid distributor");
+
+        luxfiToken = IERC20(_token);
+        feeDistributor = IFeeDistributor(_feeDistributor);
+    }
+
+    function stake(uint256 amount) external nonReentrant {
+        require(amount > 0, "Invalid amount");
+
+        luxfiToken.transferFrom(msg.sender, address(this), amount);
+
+        staked[msg.sender] += amount;
+        totalStaked += amount;
+
+        feeDistributor.registerStaker(msg.sender, amount);
+
+        emit Staked(msg.sender, amount);
+    }
+
+    function unstake(uint256 amount) external nonReentrant {
+        require(amount > 0, "Invalid amount");
+        require(staked[msg.sender] >= amount, "Insufficient stake");
+
+        staked[msg.sender] -= amount;
+        totalStaked -= amount;
+
+        if (staked[msg.sender] == 0) {
+            feeDistributor.unregisterStaker(msg.sender);
+        } else {
+            feeDistributor.registerStaker(msg.sender, staked[msg.sender]);
+        }
+
+        luxfiToken.transfer(msg.sender, amount);
+
+        emit Unstaked(msg.sender, amount);
+    }
+
+    function setFeeDistributor(address _fd) external onlyOwner {
+        require(_fd != address(0), "Invalid address");
+        feeDistributor = IFeeDistributor(_fd);
+    }
+}
+```
